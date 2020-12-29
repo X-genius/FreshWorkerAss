a93f9f268176c3c57b38c6f6ac4b75e8924817c1
Aman <55349238+X-genius@users.noreply.github.com>
2020-12-29 14:20:59 +0530

    CRD operation using java with few comments

--- App.java
+++ App.java
@@ -1,17 +1,25 @@
+//import all files of java
 import java.io.*;
 import java.util.*;
 import java.lang.String;
+
+//first to download a jar file
+//And move to reference in java projects 
+//and import it and use it
 import org.json.simple.JSONObject;
 
-
+//Create a class for doing CRD operation
 public class App {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
    
+    //Create function which doing all operations like Create, Read, Delete 
     public void insertValue() throws IOException
     {
+        //taking input using Scanner
         Scanner scn = new Scanner(System.in);
 
+        //Given Some entries of std management System
         System.out.println("Enter The Name of Student : ");
         String stdName = scn.next();
 
@@ -24,21 +32,27 @@ public class App {
         System.out.println("Enter The Subject of Student : ");
         String subStd = scn.next();
 
+        //Create JSONObject
         JSONObject obj = new JSONObject();
 
+        //Basically doing CREATE operation
         obj.put("Student_Name" , stdName);
         obj.put("Student_Age" , ageOfStd);
         obj.put("Student_Class" , classOfStd);
         obj.put("Student_Subject" , subStd);
 
+        //Read and Store on CRD.txt
         PrintWriter pWriter = new PrintWriter(new BufferedWriter(new FileWriter("CRD.txt",true)));
         pWriter.print(obj);
         System.out.println("Successfully Add Details");
+
+        //Then Close means over the data
         pWriter.close();
     }
     
     public static void main(String args[]) throws IOException
     {
+        //Create App obj and fetching data
        App obj = new App();
        obj.insertValue();
     }
